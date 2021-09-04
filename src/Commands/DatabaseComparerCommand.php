444ca408ec2c139b01139f0eabc399358b3c96f2
Dieter Coopman <dieter@deltasolutions.be>
2021-09-04 19:46:32 +0200

    start implementing migration creation

--- src/Commands/DatabaseComparerCommand.php
+++ src/Commands/DatabaseComparerCommand.php
@@ -8,6 +8,7 @@ class DatabaseComparerCommand extends Command
 {
     public $signature = 'dbcomparer:compare
                         {--sql : show an sql statement as output}
+                        {--migrations : create migrations for the comparison}
                         {--save : save the sql statements to an sql file}';
 
     public $description = 'This command syncs the structure of a target database with a source database';
@@ -25,6 +26,10 @@ class DatabaseComparerCommand extends Command
 
             if ($options['sql']) {
                 return $this->info($comparison->getSql());
+            }
+
+            if ($options['migrations']) {
+                return $comparison->saveToMigrations();
             }
 
             if ($this->confirm('Are you sure you want to sync your target database ?')) {
